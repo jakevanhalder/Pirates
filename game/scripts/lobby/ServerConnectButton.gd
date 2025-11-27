f1edf6cd9e9db27509521056e4ace10f3b00e474
jakevanhalder <124746901+jakevanhalder@users.noreply.github.com>
2025-11-26 18:18:39 -0800

    Implement base ui controls for game lobby user flow.

---
+++ game/scripts/lobby/ServerConnectButton.gd
@@ -0,0 +1,19 @@
+extends Button
+
+@onready var user_name_textbox: TextEdit = $"../TextEdit"
+@onready var user_label: Label = $"../UsernameLabel"
+
+func _on_pressed() -> void:
+	var name = user_name_textbox.text.strip_edges()
+	if name == "":
+		name = "New User"
+	
+	if self.text == "Clear":
+		self.text = "Set Name"
+		user_label.visible = false
+		user_name_textbox.visible = true
+	else:
+		self.text = "Clear"
+		user_name_textbox.visible = false
+		user_label.visible = true
+		user_label.text = "Username: " + name
